2a5c4588ec788ff6b49d67be635f39d9d8895a44
hugohoffmann <hugohoffmann04@gmail.com>
2019-05-01 18:22:40 -0300

    #2 estilização do menu

--- projeto/resources/views/layout/index.blade.php
+++ projeto/resources/views/layout/index.blade.php
@@ -8,36 +8,37 @@
      <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
      <link rel="stylesheet" href="{{ asset('css/app.css') }}">
      <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-    <title>Document</title>
+    <title>Trabalho De Des.Web </title>
 </head>
 <body>
-    <div class="row">
-        <div class="col-md-1">
-
-            <header>
-                <section>
-                    <ul>
-                        <li>Clientes</li>
-                        <li>Ordem</li>
-                        <li>Produto</li>
-                        <li>Categorias</li>
-                        <li>Ordem Detalhe</li>
-                    </ul>
-                </section>
-            </header>
+                
+    <div class="d-flex" id="wrapper">
+        <!-- Sidebar -->
+        <div class="bg-light border-right" id="sidebar-wrapper">
+            <div class="sidebar-heading"><h2>Trabalho</h2></div>
+            <div class="list-group list-group-flush">
+            <a href="/clientes" class="list-group-item list-group-item-action bg-light">Clientes</a>
+            <a href="/categorias" class="list-group-item list-group-item-action bg-light">Categorias</a>
+                <a href="/ordem" class="list-group-item list-group-item-action bg-light">Ordem</a>
+                <a href="/produto" class="list-group-item list-group-item-action bg-light">Produto</a>
+                <a href="/ordemdetalhe" class="list-group-item list-group-item-action bg-light">Ordem Detalhe</a>
+            </div>
         </div>
+        <!-- /#sidebar-wrapper -->
+    
+            
+        <section class="container-fluid">
+            <div class="row">
+                @if (session('message'))
+                    <div class="col-12 alert alert-info">
+                        {{ session('message') }}
+                    </div>
+                @endif
+            </div>
+
+            @yield('conteudo')  
+        </section>
     </div>
-    <section class="container">
-        <div class="row">
-            @if (session('message'))
-                <div class="col-12 alert alert-info">
-                    {{ session('message') }}
-                </div>
-            @endif
-        </div>
-
-        @yield('conteudo')  
-    </section>
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/custom.js') }}"></script>
 </body>
