0f8b6a10acf92f97478e2c345225052d1b108c4c
hugohoffmann <hugohoffmann04@gmail.com>
2019-05-04 17:02:10 -0300

    #4 actions inserts

--- projeto/app/Http/Controllers/OrdemController.php
+++ projeto/app/Http/Controllers/OrdemController.php
@@ -3,8 +3,69 @@
 namespace projeto\Http\Controllers;
 
 use Illuminate\Http\Request;
+use projeto\{Ordem, OrdemDetalhe};
 
 class OrdemController extends Controller
 {
-    //
+    public function listar(){
+        $ordens = Ordem::all();
+
+        return view('ordem.listar', compact('ordens'));
+    }
+
+    public function incluir(){
+        return view('ordem.incluir');
+    }
+
+    public function salvar(Request $request){
+        $id = Ordem::max('order_id');
+        $id++;
+
+        $validate = $request->validate([
+            'ship_name' => 'required'
+        ]);
+
+        $ordem = new Ordem([
+            'order_id' => $id,
+            'ship_name' => $request->get('ship_name'),
+        ]);
+
+        $ordem->save();
+
+        return redirect()->route('ordem.listar')->with('message', 'Ordem inserida com sucesso!');
+
+    }
+
+    public function alterar($id){
+        $ordem = Ordem::findOrFail($id);
+
+        return view('ordem.alterar', compact('ordem'));
+    }
+
+    public function atualizar(Request $request, $id){
+        $validate = $request->validate([
+            'ship_name' => 'required'
+        ]);
+
+        $ordem = Ordem::findOrFail($id);
+        $ordem->ship_name = $request-get('ship_name');
+
+        $ordem->save();
+
+        return redirect()->route('ordem.listar')->with('messagem', 'Ordem atualizada com sucesso!');
+
+    }
+
+    public function excluir($id){
+        $ordem = Ordem::findOrFail($id);
+
+        if (OrdemDetalhe::where('order_id', '==', $id)->count()){
+            return redirect()->route('ordem.listar')->with('message', 'É necessário excluir os produtos desse pedido antes de excluí-lo!');
+        }
+
+        $ordem->delete();
+
+        return redirect()->route('ordem.listar')->with('message', 'Ordem excluída com sucesso!');
+
+    }
 }
--- projeto/app/Ordem.php
+++ projeto/app/Ordem.php
@@ -10,4 +10,21 @@ class Ordem extends Model
     protected $primaryKey = 'order_id';
     public $timestamps = false;
     public $incrementing = false;
+
+    protected $fillabre = [
+        'order_id',
+        'customer_id',
+        'employee_id',
+        'order_date',
+        'required_date',
+        'shipped_date',
+        'ship_via',
+        'freight',
+        'ship_name',
+        'ship_address',
+        'ship_city',
+        'ship_region',
+        'ship_postal_code',
+        'ship_country'
+    ];
 }
