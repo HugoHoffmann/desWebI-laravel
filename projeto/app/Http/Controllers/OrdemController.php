6b215203bb5a44f45a959b656c0473aa6aa491c3
hugohoffmann <hugohoffmann04@gmail.com>
2019-05-05 21:27:24 -0300

    #4 controller adaptado

--- projeto/app/Http/Controllers/OrdemController.php
+++ projeto/app/Http/Controllers/OrdemController.php
@@ -8,7 +8,7 @@ use projeto\{Ordem, OrdemDetalhe, Cliente};
 class OrdemController extends Controller
 {
     public function listar(){
-        $ordens = Ordem::all();
+        $ordens = Ordem::orderBy('order_id')->get();
         
         return view('ordem.listar', compact('ordens'));
     }
@@ -24,13 +24,27 @@ class OrdemController extends Controller
         $id++;
 
         $validate = $request->validate([
-            'ship_name' => 'required'
+            'ship_name' => 'required',
+            'customer_id' => 'required'
         ]);
 
         $ordem = new Ordem([
             'order_id' => $id,
             'ship_name' => $request->get('ship_name'),
+            'order_date' => $request->get('order_date'),
+            'required_date' => $request->get('required_date'),
+            'shipped_date' => $request->get('shipped_date'),
+            'ship_via' => $request->get('ship_via'),
+            'freight' => $request->get('freight'),
+            'ship_address' => $request->get('ship_address'),
+            'ship_city' => $request->get('ship_city'),
+            'ship_region' => $request->get('ship_region'),
+            'ship_postal_code' => $request->get('ship_postal_code'),
+            'ship_country' => $request->get('ship_country'),
         ]);
+
+        $cliente = Cliente::findOrFail($request->get('customer_id'));
+        $ordem->cliente()->associate($cliente);    
 
         $ordem->save();
 
@@ -47,12 +61,25 @@ class OrdemController extends Controller
 
     public function atualizar(Request $request, $id){
         $validate = $request->validate([
-            'ship_name' => 'required'
+            'ship_name' => 'required',
+            'customer_id' => 'required'
         ]);
+        $cliente = Cliente::findOrFail($request->get('customer_id'));
 
         $ordem = Ordem::findOrFail($id);
         $ordem->ship_name = $request->get('ship_name');
+        $ordem->order_date = $request->get('order_date');
+        $ordem->required_date = $request->get('required_date');
+        $ordem->shipped_date = $request->get('shipped_date');
+        $ordem->ship_via = $request->get('ship_via');
+        $ordem->freight = $request->get('freight');
+        $ordem->ship_address = $request->get('ship_address');
+        $ordem->ship_city = $request->get('ship_city');
+        $ordem->ship_region = $request->get('ship_region');
+        $ordem->ship_postal_code = $request->get('ship_postal_code');
+        $ordem->ship_country = $request->get('ship_country');
 
+        $ordem->cliente()->associate($cliente);
         $ordem->save();
 
         return redirect()->route('ordem.listar')->with('messagem', 'Ordem atualizada com sucesso!');
